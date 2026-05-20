877463d9f00f8629047ed907680c3e05a96cbd5a
ruby4heart <ruby4heart@gmail.com>
2026-05-20 21:32:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Set your GH Pages URL here e.g.:
-ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
-ghRawURL = "https://raw.githubusercontent.com/conoro/tiktok-rss-flat/main/"
+ghPagesURL = "https://ruby4heart.github.io/tiktok-rss-flat/"
+ghRawURL = "https://raw.githubusercontent.com/ruby4heart/tiktok-rss-flat/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
